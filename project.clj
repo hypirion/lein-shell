773882efd871fc2081c19fce54d55ae08532cf5d
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-13 02:30:01 +0200

    Only deploy from stable.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://www.github.com/hyPiRion/lein-shell"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-branches ["stable"]
   :eval-in-leiningen true)
