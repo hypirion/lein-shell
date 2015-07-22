d7ee3dc879ac88b454405ff8fbcd72632f8ea8b8
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-07-22 13:36:33 +0200

    Back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-shell "0.4.1"
+(defproject lein-shell "1.0.0-SNAPSHOT"
   :description "Call shell from within Leiningen."
   :url "http://www.github.com/hyPiRion/lein-shell"
   :license {:name "Eclipse Public License"
