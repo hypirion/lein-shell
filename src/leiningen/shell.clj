b364cc581f9ab86709e3dcb5f9f6a267fd6eccb8
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-11-28 21:19:00 +0100

    Support recursive parameter expansion, closes #21
    But I'll have to write docs before a release can get out.

--- src/leiningen/shell.clj
+++ src/leiningen/shell.clj
@@ -5,20 +5,107 @@
             [leiningen.core.main :as main]
             [leiningen.core.utils :as utils]))
 
+(defn- failf [fmt-string & args]
+  (throw (Exception. ^String (apply format fmt-string args))))
+
+(declare read-expansion)
+
+(defn- read-default
+  "read-default works like replace-values, except that it will stop
+  reading when it finds a }. It returns a tuple [val end-pos], where
+  val is a string of what was read."
+  [project ^String s orig-start start]
+  (let [sb (StringBuilder.)]
+    (loop [i start
+           quoted false]
+      (if (>= i (.length s))
+        (failf "Unexpected end of argument '%s'. Opening ${ starts at %d, but is not closed"
+               s (dec orig-start))
+        (let [c (.charAt s i)]
+          (if quoted
+            (do (.append sb c)
+                (recur (inc i) false))
+            (case c
+              \$ (let [[val end-pos] (read-expansion project s (inc i))]
+                   (.append sb (str val))
+                   (recur (inc end-pos) false))
+              \} [(.toString sb) i]
+              \\ (recur (inc i) true)
+              (do (.append sb c)
+                  (recur (inc i) false)))))))))
+
+(defn- lookup-vector
+  "Given a string and a start/stop position, returns the Clojure value it
+  represents. If the value is not a vector, it is wrapped in one."
+  [^String s start end]
+  (let [lookup-str (subs s start end)
+        lookup-val (read-string lookup-str)]
+    (if-not (vector? lookup-val)
+      [lookup-val]
+      lookup-val)))
+
+(defn- read-expansion
+  "read-expansion reads a parameter expansion. It does not perform any kind of
+  quoting, and Clojure values that contain :- or } will error out the expansion
+  mechanism. For information on how the parameter expansion works, look at
+  replace-values."
+  [project ^String s start]
+  (if (not= (.charAt s start) \{)
+    (failf "Expected { after $ at position %d in argument '%s', but was %c"
+           start s (.charAt s start))
+    (loop [i (inc start)
+           colon false]
+      (if (>= i (.length s))
+        (failf "Unexpected end of argument '%s'. Opening ${ starts at %d, but is not closed"
+               s (dec start))
+        (let [c (.charAt s i)]
+          (cond (and colon (= c \-))
+                ;; ":-" -> we're done then. No smart syntactic trick here, so if you
+                ;; have a Clojure value that contains :- then you have a problem.
+                (let [lookup-vec (lookup-vector s (inc start) (dec i))
+                      [default end-pos] (read-default project s start (inc i))]
+                  [(get-in project lookup-vec default) end-pos])
+                (= c \}) ;; no support for values containing } either
+                (let [lookup-vec (lookup-vector s (inc start) i)]
+                  [(get-in project lookup-vec) i])
+                :otherwise
+                (recur (inc i) (= c \:))))))))
+
 (defn- replace-values
-  [project s]
-  (s/replace s #"\$\{.*?\}"
-             (fn [raw]
-               ;; Remove first 2 and last character
-               (let [val (read-string (subs raw 2 (dec (count raw))))]
-                 (if (vector? val)
-                   (str (get-in project val))
-                   (str (get project val)))))))
+  "replace-values takes a project and a string s. Parameter expansion is applied
+  to forms in the shape ${xxx}, where xxx is a clojure value or a vector of
+  clojure values. If the shape looks like ${xxx:-yyy}, then the string yyy will
+  be the default value if xxx does not exist in the project map. yyy will be
+  expanded recursively, i.e. the form ${xxx:-${xxx2:-yyy}} is legal, and will
+  look for xxx, then xxx2 if not found, then yyy if not found, in that order.
+
+  replace-values will also unquote backslashed values. \"\\${:foo}\"
+  will be translated to the string \"${:foo}\", and \"\\\\${:foo}\"
+  will be translated to \"\\[expansion]\".
+
+  The form ${} is illegal."
+  [project ^String s]
+  (let [sb (StringBuilder. (.length s))]
+    (loop [i (int 0)
+           quoted false]
+      (if (>= i (.length s))
+        (.toString sb)
+        (let [c (.charAt s i)]
+          (if quoted
+            (do (.append sb c)
+                (recur (inc i) false))
+            (case c
+                \$ (let [[val end-pos] (read-expansion project s (inc i))]
+                       (.append sb (str val))
+                       (recur (int (inc end-pos)) false))
+                \\ (recur (inc i) true)
+                (do (.append sb c)
+                    (recur (inc i) false)))))))))
 
 (defn- param-expand
   [project s]
   (if-not (string? s)
-    s
+    (str s)
     (replace-values project s)))
 
 (defmacro ^:private get-setting-fn
